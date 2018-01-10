b603ed107363558f5f8c8f7038f4ac4df09f5cb9
xiaohui <xiaohui@zhangxh.net>
2018-01-10 10:45:05 +0800

    print all toc menus

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,21 @@
 //= require ./all_nosearch
 //= require ./app/_search
+
+(function(){
+    var mediaQueryList = window.matchMedia('print');
+    mediaQueryList.addListener(function (mql) {
+        if (mql.matches) {
+            var elements = $('.toc-wrapper').find('.toc-list-h1, .toc-list-h2, .toc-list-h3')
+            for (var i = 0; i < elements.length; i++) {
+                var ele = elements[i];
+                if (ele.style && ele.style.display === 'none') {
+                    ele.style.display = null;
+                }
+            }
+            // console.log('onbeforeprint equivalent');
+        } else {
+            // console.log('onafterprint equivalent');
+        }
+    });
+
+})();
